9c4ce19c69807576afe7c95074bc20b58f62b79e
lopes7890 <lopesallison414@gmail.com>
2025-01-25 17:47:37 -0300

    testing config render

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-# Instala as dependências necessárias para o Puppeteer
+# Atualiza o sistema e instala as bibliotecas necessárias para o Puppeteer
 apt-get update
-apt-get install -y wget gnupg
-wget -qO- https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y libnss3 libatk-bridge2.0-0 libxcomposite1 libxdamage1 libxrandr2 libasound2 libpangocairo-1.0-0 libcups2 libxshmfence1 libgbm1
+apt-get install -y \
+  wget \
+  gnupg \
+  libnss3 \
+  libatk-bridge2.0-0 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libasound2 \
+  libpangocairo-1.0-0 \
+  libcups2 \
+  libxshmfence1 \
+  libgbm1
--- backend/src/utils/translateWords.js
+++ backend/src/utils/translateWords.js
@@ -3,14 +3,20 @@ import puppeteer from "puppeteer";
 export const translateWordsPortuguese = async (texto) => {
     try{
 
+        //await browser.close();  
+        console.log("Iniciando o Puppeteer...");
         const browser = await puppeteer.launch({
             headless: true, // Modo sem interface gráfica
             args: ['--no-sandbox', '--disable-setuid-sandbox'], // Necessário em alguns servidores
         });
 
+        console.log("Browser iniciado com sucesso.");
+
         const page = await browser.newPage()
 
         await page.goto("https://translate.google.com.br/?sl=en&tl=pt&op=translate")
+        
+        console.log("Nova página criada.");
 
   // Aguarde o seletor
         await page.waitForSelector('#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > div > c-wiz > span > span > div > textarea'); 
